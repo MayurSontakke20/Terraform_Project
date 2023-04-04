0979e69ebc56b13dc59d107606f244be06ba9b10
MayurSontakke20 <124254352+MayurSontakke20@users.noreply.github.com>
2023-04-04 20:26:04 +0530

    Automatiom

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIATTAU7W3M74WQ2CRB"
+secret_key = "V/nOliSidARl+YAfCb3oX4D8L+9bqduM/UGnhgMc"
+}
+
+resource "aws_instance" "ec2_example" {
+  ami                    = "ami-0cca134ec43cf708f"
+  instance_type          = var.instance_type
+  tags = {
+    Name = "Terraform EC2"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 40
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
